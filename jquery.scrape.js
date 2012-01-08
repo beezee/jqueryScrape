1983a4420f57ef47c9cf4b80cbb5353099d7018a
beezee <brian@streetwise-media.com>
2012-01-08 11:47:28 -0500

    isolating responses to prevent collisions

--- jquery.scrape.js
+++ jquery.scrape.js
@@ -13,11 +13,11 @@
      
        $.post(this.proxyUrl, {jqueryScrapeUrl : url}, function(response) {
          self.result = $(response);
-         if (finishedEvent) $.event.trigger(finishedEvent);
+         if (finishedEvent) $.event.trigger(finishedEvent, $(response));
        });
      },
      
-     span2img : function(options, callback) {
+     span2img : function(result, options, callback) {
       
         var settings = $.extend({
           filter : '',
@@ -27,11 +27,12 @@
         
         var self = this;
         
-        if (typeof(callback) != 'undefined') self.result.find(' span[src]').each(callback); else
-        self.result.find('span[src]'+settings.filter).each(function() {
+        if (typeof(callback) != 'undefined') $(result).find(' span[src]').each(callback); else
+        $(result).find('span[src]'+settings.filter).each(function() {
           var imgSrc = $(this).attr('src');
           $('<img class="'+settings.imgClass+'" src="'+settings.prepend+imgSrc+'" />').insertAfter($(this));
          })
+        return $(result);
      },
      
      setProxyUrl : function(url) {
